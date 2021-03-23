47a350ac547f3a1de4c0c0a1c537ac8dcfb93ea9
3038922 <31612534@qq.com>
2021-03-23 12:34:26 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- script/ChocolateyInstall.ps1
+++ script/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 # This is where you see the top level API - with xml to Packages - should look nearly the same as https://chocolatey.org/api/v2/
 # If you are using Nexus, always add the trailing slash or it won't work
 # === EDIT HERE ===
-$packageRepo = 'https://qzrobot.top/index.php/s/7NKAqJtFwCBHjTo/download'
+$packageRepo = 'https://qzrobot.top/index.php/s/7NKAqJtFwCBHjTo/download/chocolatey.0.10.15.nupkg'
 
 # If the above $packageRepo repository requires authentication, add the username and password here. Otherwise these leave these as empty strings.
 $repoUsername = ''    # this must be empty is NOT using authentication
--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
 #安装choco 
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
-iex ((New-Object System.Net.WebClient).DownloadString('https://qzrobot.top/index.php/s/C8CqBw6a2conq54/download'))
+iex ((New-Object System.Net.WebClient).DownloadString('https://qzrobot.top/index.php/s/C8CqBw6a2conq54/download/ChocolateyInstall.ps1'))
