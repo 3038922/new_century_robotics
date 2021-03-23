32b14c26998e82f2f674e966c1f0565a0a99f59d
nameVal <1729169882@qq.com>
2021-03-23 11:05:48 +0800

    update
    Signed-off-by: nameVal <1729169882@qq.com>

--- test.ps1
+++ test.ps1
@@ -1,12 +1,3 @@
-# if ((Read-Host '�Ƿ����ninja+ccls+llvm?[Y/N]') -eq 'y') {
-#     Write-Host "ѡ����" -ForegroundColor Green
-# }
-# else {
-#     Write-Host "ѡ�˷�" -ForegroundColor Green
-# }
-
-# #& msiexec /i "C:\temp\WinRAR.exe" TARGETDIR="C:\temp" /qb
-
-#c:/ncrRobotics/temp/python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-& c:/ncrRobotics/temp/code.exe /VERYSILENT /mergetasks=!runcode /ALLUSERS
-& c:/ncrRobotics/temp/Git.exe /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /ALLUSERS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"
+#安装choco 
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
+iex ((New-Object System.Net.WebClient).DownloadString('https://qzrobot.top/index.php/s/C8CqBw6a2conq54/download'))
